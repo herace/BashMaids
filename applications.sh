a26e878c4b7964bfc9a2a4910089ecb30f8bc55c
Herace Siinclaiir <her132ace@gmail.com>
2019-02-01 08:38:26 +0000

    applications.sh added
    a bash script adding all applications i use using pacman

---
+++ applications.sh
@@ -0,0 +1,1 @@
+pacman -Sy fish git geany vim ranger w3m surf sxiv mplayer zathura dmenu mpd neofetch filezilla transmission-cli htop krita gimp blender imagemagick ffmpeg wpa_supplicant-openrc scribus calcurse abiword firefox neomutt rxvt-unicode
